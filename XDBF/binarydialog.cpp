76cb5221bf0621cc3cef2a3724f2dadc283e1a5e
Stevie Hetelekides <hetelek@gmail.com>
2012-05-27 01:24:55 -0400

    added extract binary button

--- XDBF/binarydialog.cpp
+++ XDBF/binarydialog.cpp
@@ -1,8 +1,14 @@
 #include "binarydialog.h"
 #include "ui_binarydialog.h"
 
+#include <QFileDialog>
+#include <QDesktopServices>
+#include <QMessageBox>
+
 BinaryDialog::BinaryDialog(QWidget *parent, Setting_Entry *entry) : QDialog(parent), ui(new Ui::BinaryDialog)
 {
+    se = entry;
+
     setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
     ui->setupUi(this);
 
@@ -45,4 +51,28 @@ BinaryDialog::BinaryDialog(QWidget *parent, Setting_Entry *entry) : QDialog(pare
 BinaryDialog::~BinaryDialog()
 {
     delete ui;
+}
+
+void BinaryDialog::on_pushButton_clicked()
+{
+    QString desktopLocation = QDesktopServices::storageLocation(QDesktopServices::DesktopLocation);
+    QString saveLoc = QFileDialog::getSaveFileName(this, "Select a Save Path", desktopLocation);
+
+    if(saveLoc == "")
+        return;
+
+    QByteArray ba = saveLoc.toAscii();
+    char *path_c = ba.data();
+
+    FILE *f;
+    try
+    {
+        f = fopen(path_c, "wb");
+        fwrite(se->binary.data, se->binary.size, sizeof(char), f);
+        fclose(f);
+    }
+    catch(...)
+    {
+        QMessageBox::warning(this, "Error Occured", "The data could not be written to the disk.");
+    }
 }
--- XDBF/binarydialog.h
+++ XDBF/binarydialog.h
@@ -16,8 +16,12 @@ public:
     explicit BinaryDialog(QWidget *parent, Setting_Entry *entry);
     ~BinaryDialog();
     
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::BinaryDialog *ui;
+    Setting_Entry *se;
 };
 
 #endif // BINARYDIALOG_H
